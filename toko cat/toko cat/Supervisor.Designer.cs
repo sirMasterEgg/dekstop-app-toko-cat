9f25ba213f6c754a9c0ce56db3d60366cce95f54
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2022-05-17 11:50:17 +0700

    Add supervisor form and basic mechanic

---
+++ toko cat/toko cat/Supervisor.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace toko_cat
+{
+    partial class Supervisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSupervisor = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tambahJadwalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tambahJadwalToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupervisor)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvSupervisor
+            // 
+            this.dgvSupervisor.AllowUserToAddRows = false;
+            this.dgvSupervisor.AllowUserToDeleteRows = false;
+            this.dgvSupervisor.AllowUserToResizeColumns = false;
+            this.dgvSupervisor.AllowUserToResizeRows = false;
+            this.dgvSupervisor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvSupervisor.ColumnHeadersHeight = 34;
+            this.dgvSupervisor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvSupervisor.Location = new System.Drawing.Point(0, 33);
+            this.dgvSupervisor.MultiSelect = false;
+            this.dgvSupervisor.Name = "dgvSupervisor";
+            this.dgvSupervisor.ReadOnly = true;
+            this.dgvSupervisor.RowHeadersVisible = false;
+            this.dgvSupervisor.RowHeadersWidth = 62;
+            this.dgvSupervisor.RowTemplate.Height = 28;
+            this.dgvSupervisor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSupervisor.Size = new System.Drawing.Size(1570, 834);
+            this.dgvSupervisor.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tambahJadwalToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1570, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tambahJadwalToolStripMenuItem
+            // 
+            this.tambahJadwalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tambahJadwalToolStripMenuItem1,
+            this.reportToolStripMenuItem1});
+            this.tambahJadwalToolStripMenuItem.Name = "tambahJadwalToolStripMenuItem";
+            this.tambahJadwalToolStripMenuItem.Size = new System.Drawing.Size(73, 29);
+            this.tambahJadwalToolStripMenuItem.Text = "&Menu";
+            // 
+            // tambahJadwalToolStripMenuItem1
+            // 
+            this.tambahJadwalToolStripMenuItem1.Name = "tambahJadwalToolStripMenuItem1";
+            this.tambahJadwalToolStripMenuItem1.Size = new System.Drawing.Size(270, 34);
+            this.tambahJadwalToolStripMenuItem1.Text = "&Tambah Jadwal";
+            this.tambahJadwalToolStripMenuItem1.Click += new System.EventHandler(this.tambahJadwalToolStripMenuItem1_Click);
+            // 
+            // reportToolStripMenuItem1
+            // 
+            this.reportToolStripMenuItem1.Name = "reportToolStripMenuItem1";
+            this.reportToolStripMenuItem1.Size = new System.Drawing.Size(233, 34);
+            this.reportToolStripMenuItem1.Text = "&Report";
+            // 
+            // Supervisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1570, 867);
+            this.Controls.Add(this.dgvSupervisor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Supervisor";
+            this.Text = "Supervisor";
+            this.Load += new System.EventHandler(this.Supervisor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSupervisor)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSupervisor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tambahJadwalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tambahJadwalToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem1;
+    }
+}
---
+++ toko cat/toko cat/Supervisor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace toko_cat
+{
+    public partial class Supervisor : Form
+    {
+        private MySqlConnection conn = Connection.Conn;
+        private MySqlDataAdapter da;
+        private MySqlCommand cmd;
+        private DataTable dtSupervisor;
+        public Supervisor()
+        {
+            InitializeComponent();
+        }
+
+        private void Supervisor_Load(object sender, EventArgs e)
+        {
+            loadDataGrid();
+        }
+
+        private void loadDataGrid()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+            dtSupervisor = new DataTable();
+            cmd = new MySqlCommand();
+            da = new MySqlDataAdapter();
+
+            cmd.Connection = conn;
+
+            cmd.CommandText = @"select date_format(v.V_TANGGAL, '%W, %d %M %Y') as 'Jadwal Visit', t.TOKO_NAME as 'Toko Tujuan', u.US_NAME as 'Nama Sales', if(v.V_STATUS = 0, 'Belum dikunjungi', 'Sudah dikunjungi') as Status from visit v
+                join toko t on t.TOKO_ID = v.V_TOKO_ID
+                join user u on u.US_ID = v.V_US_ID";
+            conn.Open();
+            cmd.ExecuteReader();
+            conn.Close();
+            da.SelectCommand = cmd;
+            da.Fill(dtSupervisor);
+
+            dgvSupervisor.DataSource = null;
+            dgvSupervisor.DataSource = dtSupervisor;
+
+            foreach (DataGridViewColumn column in dgvSupervisor.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            
+            dgvSupervisor.ClearSelection();
+        }
+
+        private void tambahJadwalToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            SupervisorAdd add = new SupervisorAdd();
+            add.ShowDialog();
+            loadDataGrid();
+        }
+    }
+}
