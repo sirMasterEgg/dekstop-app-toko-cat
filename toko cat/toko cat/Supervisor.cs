d3295f429e637655cd25432c986fd9ab831358c5
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2022-05-22 23:30:49 +0700

    complete supervisor form

--- toko cat/toko cat/Supervisor.cs
+++ toko cat/toko cat/Supervisor.cs
@@ -17,6 +17,7 @@ namespace toko_cat
         private MySqlDataAdapter da;
         private MySqlCommand cmd;
         private DataTable dtSupervisor;
+        private DataTable dtSupervisorTemp;
         public Supervisor()
         {
             InitializeComponent();
@@ -56,7 +57,7 @@ namespace toko_cat
             conn.Close();
             da.SelectCommand = cmd;
             da.Fill(dtSupervisor);
-
+            
             dgvSupervisor.DataSource = null;
             dgvSupervisor.DataSource = dtSupervisor;
 
@@ -66,6 +67,21 @@ namespace toko_cat
             }
             
             dgvSupervisor.ClearSelection();
+
+            cmd.CommandText = @"SELECT V_ID        AS 'ID Kunjungan',
+                                       t.TOKO_NAME AS 'Toko Tujuan',
+                                       d.DA_NAME   AS 'Hari Visit',
+                                       u.US_NAME   AS 'Nama Sales',
+                                       u.US_ID     AS 'ID Sales',
+                                       IF(generateCountAbsen(u.US_ID)=0,'Belum Dikunjungi', 'Sudah Dikunjungi') AS 'Absen'
+                                FROM visit v
+                                         LEFT JOIN USER u ON u.US_ID = v.V_US_ID
+                                         LEFT JOIN toko t ON t.TOKO_ID = v.V_TOKO_ID
+                                         LEFT JOIN DAY d ON v.V_DA_ID = d.DA_ID
+                                         LEFT JOIN absen a ON u.US_ID = a.AB_US_ID
+                                WHERE (d.DA_ID + 7) % 7 = DATE_FORMAT(NOW(), '%w');";
+            dtSupervisorTemp = Connection.executeAdapter(cmd);
+
         }
 
         private void tambahJadwalToolStripMenuItem1_Click(object sender, EventArgs e)
@@ -83,37 +99,56 @@ namespace toko_cat
 
         private void dgvSupervisor_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            //int idDGV = e.RowIndex;
-            //MySqlCommand cmd;
-            //var conn = Connection.Conn;
-            //int id = Convert.ToInt32(dgvSupervisor.Rows[idDGV].Cells[0].Value);
+            int idDGV = e.RowIndex;
+            MySqlCommand cmd;
+            var conn = Connection.Conn;
+            int id = hitungAbsenID();
 
-            //if (conn.State == ConnectionState.Open)
-            //{
-            //    conn.Close();
-            //}
-            
-            //try
-            //{
-            //    cmd = new MySqlCommand();
-            //    cmd.Connection = conn;
-            //    cmd.CommandText = "update visit set V_TOKO_ID = @tokoid, V_DA_ID = @day, V_US_ID = @userid where V_ID = @id;";
-            //    cmd.Parameters.Add(new MySqlParameter("@id", id));
-            //    cmd.Parameters.Add(new MySqlParameter("@tokoid", idToko));
-            //    cmd.Parameters.Add(new MySqlParameter("@day", tujuan));
-            //    cmd.Parameters.Add(new MySqlParameter("@userid", idSales));
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
 
-            //    conn.Open();
-            //    cmd.ExecuteNonQuery();
-            //    conn.Close();
+            try
+            {
+                cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "insert into absen (AB_ID, AB_US_ID, AB_DATE) values (@id, @userid, now());";
+                cmd.Parameters.Add(new MySqlParameter("@id", id));
+                cmd.Parameters.Add(new MySqlParameter("@userid", dtSupervisorTemp.Rows[idDGV]["ID Sales"].ToString()));
 
-            //    MessageBox.Show("Berhasil Diubah!");
-            //    clear();
-            //}
-            //catch (Exception excep)
-            //{
-            //    MessageBox.Show(excep.Message);
-            //}
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Sudah Absen!");
+                loadDataGrid();
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+            }
+        }
+
+        private int hitungAbsenID()
+        {
+            var conn = Connection.Conn;
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+
+            var cmd = new MySqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+
+            cmd.CommandText = "select max(AB_ID) from absen;";
+
+            string lastId = cmd.ExecuteScalar().ToString();
+
+            conn.Close();
+            return (lastId == "") ? 1 : Int32.Parse(lastId) + 1;
         }
     }
 }
